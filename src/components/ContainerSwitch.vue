af535cd0c905f3c02302105909cb03df9bb2fa7e
demo-hub <joaop86@hotmail.com>
2021-02-22 11:49:04 +0000

    Change watcher to on change method

--- src/components/ContainerSwitch.vue
+++ src/components/ContainerSwitch.vue
@@ -1,6 +1,7 @@
 <template>
     <v-switch
       v-model="running"
+      v-on:change="changeRunning()" 
       :label="`Gateway is ${running ? 'up' : 'down'}`"
       color="black"
     ></v-switch>
@@ -14,70 +15,66 @@
 
     data() {
         return {
-            running: false,
-            switchChanges: 0
+            running: false
         }
     },
 
     watch:{
-        running(value) {
-        // called whenever the switch changes
-        if (this.switchChanges > 0){
-            if (value) {
-            exec("docker-compose -f gateway/docker-compose.yml up --build -d", (error, data, getter) => {
-                if (error) {
-                    console.log("error", error.message);
-                    this.running = false
-                    return;
-                }
-                if (getter) {
-                    console.log(data)
-                    return;
-                }
-                console.log("compose up", data);
-            });
-            } else{
-            exec("docker-compose -f gateway/docker-compose.yml down --rmi all", (error, data, getter) => {
-                if (error) {
-                    console.log("error", error.message);
-                    this.running = true
-                    return;
-                }
-                if (getter) {
-                    console.log(data)
-                    return;
-                }
-                console.log("compose down", data);
-            });
-            } 
-        }  
-
-        this.switchChanges++
-        } 
+         
     },
 
     methods: {
         checkDocker() {
-        exec("docker ps -q -f name=gateway_server_1", (error, data, getter) => {
-            if (error) {
-                console.log("error", error.message);
+            exec("docker ps -q -f name=gateway_server_1", (error, data, getter) => {
+                if (error) {
+                    console.log("error", error.message);
+                    return;
+                }
+                if (getter) {
+                if (data) {
+                    this.running = true
+                } else {
+                    this.running = false
+                } 
                 return;
-            }
-            if (getter) {
-            if (data) {
+                }
+                if (data) {
                 this.running = true
-            } else {
+                } else {
                 this.running = false
+                }
+                // console.log("build", data);
+            });
+        },
+        changeRunning() {
+            // called whenever the switch changes
+            if (this.running) {
+                exec("docker-compose -f gateway/docker-compose.yml up --build -d", (error, data, getter) => {
+                    if (error) {
+                        console.log("error", error.message);
+                        this.running = false
+                        return;
+                    }
+                    if (getter) {
+                        console.log(data)
+                        return;
+                    }
+                    console.log("compose up", data);
+                });
+            } else{
+                exec("docker-compose -f gateway/docker-compose.yml down --rmi all", (error, data, getter) => {
+                    if (error) {
+                        console.log("error", error.message);
+                        this.running = true
+                        return;
+                    }
+                    if (getter) {
+                        console.log(data)
+                        return;
+                    }
+                    console.log("compose down", data);
+                });
             } 
-            return;
-            }
-            if (data) {
-            this.running = true
-            } else {
-            this.running = false
-            }
-            // console.log("build", data);
-        });
         } 
     },
 
