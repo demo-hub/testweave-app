cca8bcceb5d4d9d3cac1d9df324fa18e507a45e1
demo-hub <joaop86@hotmail.com>
2021-02-22 11:10:53 +0000

    Separate container switch logic to another component

--- src/App.vue
+++ src/App.vue
@@ -29,96 +29,19 @@
     </v-app-bar>
 
     <v-main>
-      <v-switch
-      v-model="running"
-      :label="`Gateway is ${running ? 'up' : 'down'}`"
-      color="black"
-    ></v-switch>
+      <ContainerSwitch/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-const exec = require('child_process').exec;
+import ContainerSwitch from './components/ContainerSwitch.vue'
 
 export default {
   name: 'App',
 
   components: {
-
-  },
-
-  data() {
-    return {
-      running: false,
-      switchChanges: 0
-    }
-  },
-
-  watch:{
-    running(value) {
-      // called whenever the switch changes
-      if (this.switchChanges > 0){
-        if (value) {
-          exec("docker-compose -f gateway/docker-compose.yml up --build -d", (error, data, getter) => {
-              if (error) {
-                  console.log("error", error.message);
-                  this.running = false
-                  return;
-              }
-              if (getter) {
-                console.log(data)
-                return;
-              }
-              console.log("compose up", data);
-          });
-        } else{
-          exec("docker-compose -f gateway/docker-compose.yml down --rmi all", (error, data, getter) => {
-              if (error) {
-                  console.log("error", error.message);
-                  this.running = true
-                  return;
-              }
-              if (getter) {
-                console.log(data)
-                return;
-              }
-              console.log("compose down", data);
-          });
-        } 
-      }  
-
-      this.switchChanges++
-    } 
-  },
-
-  methods: {
-    checkDocker() {
-      exec("docker ps -q -f name=gateway_server_1", (error, data, getter) => {
-        if (error) {
-            console.log("error", error.message);
-            return;
-        }
-        if (getter) {
-          if (data) {
-            this.running = true
-          } else {
-            this.running = false
-          } 
-          return;
-        }
-        if (data) {
-          this.running = true
-        } else {
-          this.running = false
-        }
-        // console.log("build", data);
-      });
-    } 
-  },
-
-  mounted() {
-    this.checkDocker();
+    ContainerSwitch
   } 
 };
 </script>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -8,9 +8,9 @@ export default new Vuetify({
     theme: {
         themes: {
           light: {
-            primary: colors.grey.base, // #E53935
-            secondary: colors.red.lighten4, // #FFCDD2
-            accent: colors.indigo.base, // #3F51B5
+            primary: colors.grey.base,
+            secondary: colors.red.lighten4,
+            accent: colors.indigo.base,
           },
         },
       }
